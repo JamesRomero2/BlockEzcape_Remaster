c6fbdb18444a91117c7db85c7df62e86923e8132
unknown <haymeemotoh22@gmail.com>
2022-12-09 19:28:40 +0800

    Some Fixes

--- Components/WorldMapComponents/LevelInformation/LevelInformation.gd
+++ Components/WorldMapComponents/LevelInformation/LevelInformation.gd
@@ -3,8 +3,18 @@ extends CanvasLayer
 func _showLevelInfo(levelNumber, levelTitle):
 	if levelNumber == -1:
 		$Control.visible = false
+	elif levelNumber == 0:
+		$Control.visible = true
+		$Control/Info.visible = false
+		
+		if levelNumber < 10:
+			$Control/VBoxContainer/LevelNumber.text = "Level 0" + str(levelNumber)
+		else:
+			$Control/VBoxContainer/LevelNumber.text = "Level " + str(levelNumber)
+		$Control/VBoxContainer/LevelTitle.text = levelTitle
 	else:
 		$Control.visible = true
+		$Control/Info.visible = true
 		var levelNum = str(levelNumber)
 		var record = GameManager._getLevelRecord(levelNumber)
 		if record[1] == 0.0:
